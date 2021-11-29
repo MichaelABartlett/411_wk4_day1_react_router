db434ba418ec94a6785c64e20e3a29158e84a6c8
DESKTOP-AONL5AH\owner <michaelbartlett864@gmail.com>
2021-11-28 21:35:50 -0600

    saving to turn in

--- src/Router.js
+++ src/Router.js
@@ -9,9 +9,22 @@ Remember to export the component at the end
 */
 
 import React from 'react'
-import { Switch, Route } from 'react-router'
+// import { Switch, Route } from 'react-router'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home'
+import About from './components/About'
+import Cars from './components/Car'
 // Write component imports here //
 
+export const Router = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="car/:id" element={<Cars />} />
+      </Routes>
+    )
+}
 
 
 // Start Router function here //
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import cars from '../cars.json'
+import { useParams } from "react-router-dom";
+import './car.css'
 // import material ui components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    let {id} = useParams();
+
+    const findCar = cars.find((car) => car.id === Number(id))
+
+    console.log(findCar)
     return (
-        <h1>A specific car</h1>
+        <div class='theList'>
+            <h1>{findCar.Name}</h1>
+            <span>{findCar.Name.toUpperCase()}</span>
+                            <ul>
+                                <li>Acceleration: {findCar["Acceleration"]}</li>  
+                                <li>Cylinders: {findCar["Cylinders"]}</li>
+                                <li>Displacement: {findCar["Displacement"]}</li>
+                                <li>Horsepower: {findCar["Horsepower"]}</li>
+                                <li>Miles_per_Gallon: {findCar["Miles_per_Gallon"]}</li>
+                                <li>Origin: {findCar["Origin"]}</li>
+                                <li>Weight_in_lbs: {findCar["Weight_in_lbs"]}</li>
+                                <li>Year: {findCar["Year"]}</li>
+                            </ul>
+        </div>                    
     )
 }
 
